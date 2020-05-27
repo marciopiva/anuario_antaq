7de33aa30dc6919b396c9ef7c1f12d3882739ada
marciopiva <marcio.piva@gmail.com>
2020-05-27 19:15:23 -0300

    dataset with codes

---
+++ src/insert_cds.sh
@@ -0,0 +1,14 @@
+sqlite3 ../db/antaq.db < insert_cd_ano.txt
+sqlite3 ../db/antaq.db < insert_cd_berco.txt
+sqlite3 ../db/antaq.db < insert_cd_cdmercadoria.txt
+sqlite3 ../db/antaq.db < insert_cd_cdtup.txt
+sqlite3 ../db/antaq.db < insert_cd_mes.txt
+sqlite3 ../db/antaq.db < insert_cd_naturezacarga.txt
+sqlite3 ../db/antaq.db < insert_cd_origemdestino.txt
+sqlite3 ../db/antaq.db < insert_cd_portoatracacao.txt
+sqlite3 ../db/antaq.db < insert_cd_sentido.txt
+sqlite3 ../db/antaq.db < insert_cd_terminal.txt
+sqlite3 ../db/antaq.db < insert_cd_tiponavegacaoatracacao.txt
+sqlite3 ../db/antaq.db < insert_cd_tipooperacao.txt
+
+
